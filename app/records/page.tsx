dbb417673105697f57fafb5c6b4156e5c4cb8c60
FalloutFalcon <falloutfalconplays@gmail.com>
2023-11-07 19:02:40 -0600

    im trying

--- app/records/page.tsx
+++ app/records/page.tsx
@@ -2,39 +2,10 @@
 import React, { useState } from "react";
 import Link from "next/link";
 
-interface Employee {
-  id: number;
-  name: string;
-  position: string;
-  department: string;
-  description: string;
-}
+import { getEmployeesData } from "./employeeData"; // Update the path accordingly
 
 export default function RecordsPage() {
-  const [employees] = useState<Employee[]>([
-    {
-      id: 1,
-      name: "Baxter Baxter",
-      position: "Engineer",
-      department: "Engineering",
-      description: "Baxter Baxter is an experienced engineer with a focus on problem-solving skills.",
-    },
-    {
-      id: 2,
-      name: "Bridget Baskket",
-      position: "CMO",
-      department: "Medical",
-      description: "Bridget Baskket is the Chief Medical Officer and leads initiatives in the medical field.",
-    },
-    {
-      id: 3,
-      name: "Click now for honks",
-      position: "Clown",
-      department: "Service",
-      description: "Click now for honks is a professional clown who brings joy and laughter to various events and parties.",
-    },
-  ]);
-
+  const employees = getEmployeesData();
   return (
     <main className="flex flex-col items-center justify-center text-center">
       <h1 className="p-5">Employee Records</h1>
--- public/employeeMarkdown/Baxter_Baxter.md
+++ public/employeeMarkdown/Baxter_Baxter.md
@@ -1,7 +1,8 @@
-# Employee: Baxter Baxter
-
-**Position:** Engineer
-
-**Department:** Engineering
+---
+employeeid: 272456232
+title: 'Employee: Baxter Baxter'
+position: 'Engineer'
+department: 'Engineering'
+---
 
 Baxter Baxter is an experienced engineer with a focus on software development and problem-solving skills. He has a strong background in various programming languages and is known for his attention to detail and analytical mindset.
