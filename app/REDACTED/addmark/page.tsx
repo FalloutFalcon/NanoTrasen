92d487f2e8549d7fbc162b661e884ea14ae76819
fallcon <falloutfalconplays@gmail.com>
2023-11-27 08:50:53 -0600

    uh shrug database i guess

---
+++ app/REDACTED/addmark/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { useState } from "react";
+import { NextPage } from "next";
+
+const AddBlogPage: NextPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    affiliation: "",
+    currentShip: "",
+    department: "",
+    position: "",
+    age: "",
+    dob: "",
+    species: "",
+    gender: "",
+    relationship: "",
+    height: "",
+    weight: "",
+    description: "",
+    ooc: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/addMark", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to submit data");
+      }
+
+      alert("Blog entry submitted successfully!");
+      // Reset form after successful submission
+      setFormData({
+        name: "",
+        affiliation: "",
+        currentShip: "",
+        department: "",
+        position: "",
+        age: "",
+        dob: "",
+        species: "",
+        gender: "",
+        relationship: "",
+        height: "",
+        weight: "",
+        description: "",
+        ooc: "",
+      });
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to submit blog entry");
+    }
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  return (
+    <div className="flex flex-col text-center">
+      <h1>Add Mark Entry</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <div>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="accent-transparent m-1  border border-mnps-blue bg-[gray] p-1"
+          />
+        </div>
+        <label>Affiliation:</label>
+        <div>
+          <input
+            type="text"
+            name="affiliation"
+            value={formData.affiliation}
+            onChange={handleChange}
+            className="accent-transparent m-1  border border-mnps-blue bg-[gray] p-1"
+          />
+        </div>
+        <label>Date of birth:</label>
+        <div>
+          <input
+            type="text"
+            name="birth"
+            value={formData.dob}
+            onChange={handleChange}
+            className="accent-transparent m-1  border border-mnps-blue bg-[gray] p-1"
+          />
+        </div>
+
+        <label>Content:</label>
+        <div>
+          <textarea
+            name="content"
+            value={formData.description}
+            onChange={handleChange}
+            className="accent-transparent m-1 border border-mnps-blue bg-[gray] p-1"
+          />
+        </div>
+        <button
+          type="submit"
+          className="p-2 w-1/12 text-lg text-[white] border-[#006f95] bg-mnps-blue border-r hover:bg-[#80bfd5] hover:text-[#818181]"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddBlogPage;
--- app/api/addmark/route.tsx
+++ app/api/addmark/route.tsx
@@ -1,41 +1,22 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const {
-    name,
-    affiliation,
-    currentShip,
-    department,
-    position,
-    age,
-    dob,
-    species,
-    gender,
-    relationship,
-    height,
-    weight,
-    description,
-    ooc,
-  } = Object.fromEntries(searchParams);
-
+export async function POST(request: Request) {
   try {
-    if (!name || !affiliation) throw new Error('Name and affiliation are required');
-    
-    // Perform the INSERT operation including all fields
+    const { name, affiliation, currentShip, department, position, age, dob, species, gender, relationship, height, weight, description, ooc } = await request.json();
+
+    if (!name) {
+      throw new Error('They need a name');
+    }
+
     await sql`
-      INSERT INTO Marks (Name, Affiliation, CurrentShip, Department, Position, Age, Dob, Species, Gender, Relationship, Height, Weight, Description, Ooc)
+      INSERT INTO Blogs (name, affiliation, currentShip, department, position, age, dob, species, gender, relationship, height, weight, description, ooc)
       VALUES (${name}, ${affiliation}, ${currentShip}, ${department}, ${position}, ${age}, ${dob}, ${species}, ${gender}, ${relationship}, ${height}, ${weight}, ${description}, ${ooc});
     `;
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
 
-  try {
-    // Fetch all entries in the Marks table after insertion
-    const marks = await sql`SELECT * FROM Marks;`;
-    return NextResponse.json({ marks }, { status: 200 });
+    const blogs = await sql`SELECT * FROM Marks;`;
+
+    return NextResponse.json({ marks }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
