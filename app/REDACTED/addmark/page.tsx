010f102fdbb23671d149930f37ed9a10f27af8e4
fallcon <falloutfalconplays@gmail.com>
2023-11-27 09:33:39 -0600

    manuel fields to see if thats the issue

--- app/REDACTED/addmark/page.tsx
+++ app/REDACTED/addmark/page.tsx
@@ -71,20 +71,146 @@ const AddMarkPage: NextPage = () => {
     <div className="flex flex-col text-center">
       <h1>Add Mark Entry</h1>
       <form onSubmit={handleSubmit}>
-        {Object.keys(formData).map((key) => (
-          <div key={key}>
-            <label>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
-            <div>
-              <input
-                type="text"
-                name={key}
-                value={formData[key as keyof typeof formData]}
-                onChange={handleChange}
-                className="accent-transparent text-[grey] m-1 p-1"
-              />
-            </div>
-          </div>
-        ))}
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.name}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.affiliation}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.currentShip}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.department}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.position}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.age}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.dob}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.species}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.gender}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.relationship}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.height}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.weight}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.description}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
+        <label>Title:</label>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={formData.ooc}
+            onChange={handleChange}
+            className="accent-transparent m-1 p-1"
+          />
+        </div>
         <button
           type="submit"
           className="p-2 w-1/12 text-lg text-[white] border-[#006f95] bg-mnps-blue border-r hover:bg-[#80bfd5] hover:text-[#818181]"
