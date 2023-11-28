983ad8dfd531509a1133c4a70b7b0b12819362b6
fallcon <falloutfalconplays@gmail.com>
2023-11-28 09:58:20 -0600

    oops uh reinvented a css class..

--- app/REDACTED/viewmarks/page.tsx
+++ app/REDACTED/viewmarks/page.tsx
@@ -4,9 +4,38 @@ import Link from "next/link";
 
 export const revalidate = 0;
 
-export default async function TablePage() {
-  //WTF why does it only work if i have this WHERE statment!! THATS NOT HOW FUCKIN ITS MEANT TO WORK!!!!!!!!!!!
+async function fetchMarksFromDatabase() {
   const marks = await sql`SELECT * FROM Marks WHERE name != 'aaaa'`;
+  return marks;
+}
+
+function renderTableRow(mark: QueryResultRow) {
+  let reusedStyle = "border border-slate-500 line-clamp-1 md:w-1/5";
+  return (
+    <tr key={mark.id} className="break-all md:break-normal flex flex-row">
+      <td className={`${reusedStyle} w-1/6`}>
+        {mark.id}
+      </td>
+      <td className={`${reusedStyle} w-1/2 md:w-1/5 break-normal text-solgov-yellow-dark hover:text-solgov-yellow`}>
+        <Link href={`/REDACTED/marks/${mark.id}`}>
+          <p>{mark.name}</p>
+        </Link>
+      </td>
+      <td className={`${reusedStyle} hidden md:w-1/5 md:table-cell`}>
+        {mark.position}
+      </td>
+      <td className={`${reusedStyle} hidden md:w-1/5 md:table-cell`}>
+        {mark.department}
+      </td>
+      <td className={`${reusedStyle} w-1/3 md:w-1/5 text-xs md:text-base`}>
+        {mark.affiliation}
+      </td>
+    </tr>
+  );
+}
+
+export default async function TablePage() {
+  const marks = await fetchMarksFromDatabase();
 
   return (
     <main className="flex flex-col items-center text-center p-5 w-4/5 m-auto">
@@ -28,30 +57,7 @@ export default async function TablePage() {
         </thead>
         <tbody>
           {marks.rows &&
-            marks.rows.map((mark: QueryResultRow) => (
-              <tr
-                key={mark.id}
-                className="break-all md:break-normal flex flex-row"
-              >
-                <td className="border border-slate-500 line-clamp-1 w-1/6 md:w-1/5">
-                  {mark.id}
-                </td>
-                <td className="border border-slate-500 line-clamp-1 w-1/2 md:w-1/5 break-normal text-solgov-yellow-dark hover:text-solgov-yellow">
-                  <Link href={`/REDACTED/marks/${mark.id}`}>
-                    <p>{mark.name}</p>
-                  </Link>
-                </td>
-                <td className="border border-slate-500 line-clamp-1 hidden md:w-1/5 md:table-cell">
-                  {mark.position}
-                </td>
-                <td className="border border-slate-500 line-clamp-1 hidden md:w-1/5 md:table-cell">
-                  {mark.department}
-                </td>
-                <td className="border border-slate-500 line-clamp-1 w-1/3 md:w-1/5 text-xs md:text-base">
-                  {mark.affiliation}
-                </td>
-              </tr>
-            ))}
+            marks.rows.map((mark: QueryResultRow) => renderTableRow(mark))}
         </tbody>
       </table>
     </main>
