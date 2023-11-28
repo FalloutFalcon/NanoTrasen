e46f520e9e9bf61002e048bde3e31110e4bba9e2
fallcon <falloutfalconplays@gmail.com>
2023-11-28 08:29:24 -0600

    waht about this

--- app/REDACTED/viewmarks/page.tsx
+++ app/REDACTED/viewmarks/page.tsx
@@ -12,27 +12,27 @@ export default async function TablePage() {
     <main className="flex flex-col items-center text-center p-5 w-4/5 m-auto">
       <table className="border border-slate-500 w-1/2 md:w-4/5">
         <thead>
-          <tr>
-            <th className="border border-slate-500">ID</th>
-            <th className="border border-slate-500">Name</th>
-            <th className="border border-slate-500">Position</th>
-            <th className="border border-slate-500">Department</th>
-            <th className="border border-slate-500">Affiliation</th>
+          <tr className="flex flex-row">
+            <th className="border border-slate-500 w-1/5">ID</th>
+            <th className="border border-slate-500 w-1/5">Name</th>
+            <th className="border border-slate-500 w-1/5">Position</th>
+            <th className="border border-slate-500 w-1/5">Department</th>
+            <th className="border border-slate-500 w-1/5">Affiliation</th>
           </tr>
         </thead>
         <tbody>
           {marks.rows &&
             marks.rows.map((mark: QueryResultRow) => (
-              <tr key={mark.id} className="break-all md:break-normal">
-                <td className="border border-slate-500">{mark.id}</td>
-                <td className="border border-slate-500 break-normal text-solgov-yellow-dark hover:text-solgov-yellow">
+              <tr key={mark.id} className="break-all md:break-normal flex flex-row">
+                <td className="border border-slate-500 w-1/5">{mark.id}</td>
+                <td className="border border-slate-500 w-1/5 break-normal text-solgov-yellow-dark hover:text-solgov-yellow">
                   <Link href={`/REDACTED/marks/${mark.id}`}>
                     <p>{mark.name}</p>
                   </Link>
                 </td>
-                <td className="border border-slate-500 text-xs md:text-base">{mark.position}</td>
-                <td className="border border-slate-500 text-xs md:text-base">{mark.department}</td>
-                <td className="border border-slate-500 text-xs md:text-base">{mark.affiliation}</td>
+                <td className="border border-slate-500 w-1/5 text-xs md:text-base">{mark.position}</td>
+                <td className="border border-slate-500 w-1/5 text-xs md:text-base">{mark.department}</td>
+                <td className="border border-slate-500 w-1/5 text-xs md:text-base">{mark.affiliation}</td>
               </tr>
             ))}
         </tbody>
