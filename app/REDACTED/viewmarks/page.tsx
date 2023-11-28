654c102442d24e89c5bd03f843dd2f1ff77b8122
fallcon <falloutfalconplays@gmail.com>
2023-11-28 08:53:15 -0600

    horrible

--- app/REDACTED/viewmarks/page.tsx
+++ app/REDACTED/viewmarks/page.tsx
@@ -13,26 +13,26 @@ export default async function TablePage() {
       <table className="border border-slate-500 w-1/2 md:w-4/5">
         <thead>
           <tr className="flex flex-row">
-            <th className="border border-slate-500 w-1/5">ID</th>
-            <th className="border border-slate-500 w-1/5">Name</th>
-            <th className="border border-slate-500 w-1/5">Position</th>
-            <th className="border border-slate-500 w-1/5">Department</th>
-            <th className="border border-slate-500 w-1/5">Affiliation</th>
+            <th className="border border-slate-500 w-1/3 md:w-1/5">ID</th>
+            <th className="border border-slate-500 w-1/3 md:w-1/5" >Name</th>
+            <th className="border border-slate-500 hidden md:w-1/5 md:table-cell">Position</th>
+            <th className="border border-slate-500 hidden md:w-1/5 md:table-cell">Department</th>
+            <th className="border border-slate-500 w-1/3 md:w-1/5">Affiliation</th>
           </tr>
         </thead>
         <tbody>
           {marks.rows &&
             marks.rows.map((mark: QueryResultRow) => (
               <tr key={mark.id} className="break-all md:break-normal flex flex-row">
-                <td className="border border-slate-500 w-1/5">{mark.id}</td>
-                <td className="border border-slate-500 w-1/5 break-normal text-solgov-yellow-dark hover:text-solgov-yellow">
+                <td className="border border-slate-500 line-clamp-1 w-1/3 md:w-1/5">{mark.id}</td>
+                <td className="border border-slate-500 line-clamp-1 w-1/3 md:w-1/5 break-normal text-solgov-yellow-dark hover:text-solgov-yellow">
                   <Link href={`/REDACTED/marks/${mark.id}`}>
                     <p>{mark.name}</p>
                   </Link>
                 </td>
-                <td className="border border-slate-500 w-1/5 text-xs md:text-base">{mark.position}</td>
-                <td className="border border-slate-500 w-1/5 text-xs md:text-base">{mark.department}</td>
-                <td className="border border-slate-500 w-1/5 text-xs md:text-base">{mark.affiliation}</td>
+                <td className="border border-slate-500 line-clamp-1 hidden md:w-1/5 md:table-cell text-xs md:text-base">{mark.position}</td>
+                <td className="border border-slate-500 line-clamp-1 hidden md:w-1/5 md:table-cell text-xs md:text-base">{mark.department}</td>
+                <td className="border border-slate-500 line-clamp-1 w-1/3 md:w-1/5 text-xs md:text-base">{mark.affiliation}</td>
               </tr>
             ))}
         </tbody>
