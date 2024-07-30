f6eec8278c2009ead99ab0b307b09efbd3345216
FalloutFalcon <falloutfalconplays@gmail.com>
2024-07-30 12:18:48 -0500

    moves it to a comp

--- app/REDACTED/relation/[slug]/page.tsx
+++ app/REDACTED/relation/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default async function markRelationPage({
   const markSlug = decodeURI(params.slug as string);
   const response = await axios.get("http://localhost:3000/api/sheetdata");
   const data = response.data;
-  const markData: FormattedMarkRelations = data[markSlug] || [];
+  const markData = data[markSlug] || [];
 
   return (
     <main className="flex flex-col items-center text-center p-5 w-4/5 m-auto">
@@ -25,26 +25,46 @@ export default async function markRelationPage({
             <th className="border border-slate-500">Their Opinion</th>
           </tr>
         </thead>
-        <tbody>
-          {markData &&
-            markData.map((relation: Relationship, index: string) => (
-              <tr key={index}>
-                <td className="border border-slate-500 text-solgov-yellow-dark hover:text-solgov-yellow">
-                  <Link href={`/REDACTED/relation/${relation.character}`}>
-                    <p>{relation.character}</p>
-                  </Link>
-                </td>
-                <td className="border border-slate-500">{relation.opinion}</td>
-                <td className="border border-slate-500">
-                  {data[relation.character]?.find(
-                    (markRelationship: Relationship) =>
-                      markRelationship.character === markSlug
-                  )?.opinion || "N/A"}
-                </td>
-              </tr>
-            ))}
-        </tbody>
+        <MarkRelationComponent
+          markData={markData}
+          data={data}
+          markSlug={markSlug}
+        />
       </table>
     </main>
   );
 }
+
+interface ComponentProps {
+  markData: Relationship[]; // Array of Relationship
+  data: { [key: string]: Relationship[] }; // Dictionary of arrays of Relationship
+  markSlug: string;
+}
+
+const MarkRelationComponent: React.FC<ComponentProps> = ({
+  markData,
+  data,
+  markSlug,
+}) => {
+  return (
+    <tbody>
+      {markData &&
+        markData.map((relation: Relationship, index: number) => (
+          <tr key={index}>
+            <td className="border border-slate-500 text-solgov-yellow-dark hover:text-solgov-yellow">
+              <Link href={`/REDACTED/relation/${relation.character}`}>
+                <p>{relation.character}</p>
+              </Link>
+            </td>
+            <td className="border border-slate-500">{relation.opinion}</td>
+            <td className="border border-slate-500">
+              {data[relation.character]?.find(
+                (markRelationship: Relationship) =>
+                  markRelationship.character === markSlug
+              )?.opinion || "N/A"}
+            </td>
+          </tr>
+        ))}
+    </tbody>
+  );
+};
